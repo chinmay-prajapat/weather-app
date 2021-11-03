609f97ab55997fb96adaa94df875f5e095134e06
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-03 07:28:09 +0530

    another property added

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // const request = require("request"); //Since request package has been deprecated
 const request = require("postman-request");
 const url =
-  "http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=23.7785179,73.6874068&units=f";
+  "http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=23.7785179,73.6874068&units=f"; //units=f(f=fahernheit)
 request({ url: url, json: true }, (err, res) => {
   //   console.log(res.body);
   console.log(
-    `It is currently ${res.body.current.temperature} degrees out. There is a ${res.body.current.precip}% chance of rain `
+    `${res.body.current.weather_descriptions[0]}. It is currently ${res.body.current.temperature} degrees out. There is a ${res.body.current.precip}% chance of rain `
   );
 });
