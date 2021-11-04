88bc0639f7d5dc9538e8b0383a7734b5d79d1c57
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-04 10:01:47 +0530

    display data

---
+++ utils/geocode.js
@@ -0,0 +1,25 @@
+const request = require("postman-request");
+const gecode = (address, callback) => {
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+    address
+  )}.json?access_token=pk.eyJ1Ijoia2lsdmlzaCIsImEiOiJja3ZpdmdmaTcwbjV1MnBuMGx1bjgzdWJlIn0.7tV-mCgZ-rb_fzrHMVvktw&limit=1`;
+  request({ url: url, json: true }, (err, res) => {
+    if (err) {
+      callback("Unable to connect with services", undefined);
+    } else if (res.body.features.length === 0) {
+      callback("The search place not found", undefined);
+    } else {
+      callback(
+        undefined,
+        res.body.features[0].center[0] +
+          "\n" +
+          res.body.features[0].center[1] +
+          "\n" +
+          res.body.features[0].place_name
+      );
+    }
+  });
+};
+module.exports = {
+  gecode: gecode,
+};
