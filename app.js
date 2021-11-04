2715e00aeeae7b9894cfef3b2dd2590d0c577f9e
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-04 22:12:16 +0530

    util function added

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
 // const request = require("request"); //Since request package has been deprecated
-const request = require("postman-request");
-// const url =
-//   "http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=23.7785179,73.6874068&units=f"; //units=f(f=fahernheit)
-// request({ url: url, json: true }, (err, res) => {
-//   //   console.log(res.body);
-//   if (res) {
-//     console.log(
-//       `${res.body.current.weather_descriptions[0]}. It is currently ${res.body.current.temperature} degrees out. There is a ${res.body.current.precip}% chance of rain `
-//     );
-//   } else {
-//     console.log(err);
-//   }
-// });
+const gecode = require("./utils/geocode");
+
 // const geocodeURL =
 //   "https://api.mapbox.com/geocoding/v5/mapbox.places/dungarpur.json?access_token=pk.eyJ1Ijoia2lsdmlzaCIsImEiOiJja3ZpdmdmaTcwbjV1MnBuMGx1bjgzdWJlIn0.7tV-mCgZ-rb_fzrHMVvktw&limit=1";
 
@@ -27,8 +16,10 @@ const request = require("postman-request");
 //   }
 // });
 
-const gecode = require("./utils/geocode");
 gecode.gecode("ahmedabad", (error, data) => {
-  console.log(error);
   console.log(data);
+});
+
+gecode.weatherForecast(23.03, 72.58, (error, data) => {
+  console.log("data", data);
 });
--- utils/geocode.js
+++ utils/geocode.js
@@ -1,4 +1,21 @@
 const request = require("postman-request");
+const weatherForecast = (lat, long, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=${lat},${long}`; //units=f(f=fahernheit)
+  request({ url: url, json: true }, (err, res) => {
+    //   console.log(res.body);
+    if (err) {
+      callback("unable to connect", undefined);
+    } else if (res.body.current === "") {
+      callback("wrong input", undefined);
+    } else {
+      callback(
+        undefined,
+        `${res.body.current.weather_descriptions[0]}. It is currently ${res.body.current.temperature} degrees out. There is a ${res.body.current.precip}% chance of rain ${res.body.location.name}`
+      );
+    }
+  });
+};
+
 const gecode = (address, callback) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
     address
@@ -21,4 +38,5 @@ const gecode = (address, callback) => {
 };
 module.exports = {
   gecode: gecode,
+  weatherForecast: weatherForecast,
 };
