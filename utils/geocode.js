c82bc2447eacfc9d0ecbacbd75e46671bbdde796
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-06 06:17:06 +0530

    es6 destructuring

--- utils/geocode.js
+++ utils/geocode.js
@@ -4,7 +4,7 @@ const gecode = (address, callback) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
     address
   )}.json?access_token=${process.env.REACT_APP_MAPBOX}&limit=1`;
-  request({ url: url, json: true }, (err, res) => {
+  request({ url, json: true }, (err, res) => {
     if (err) {
       callback("Unable to connect with services", undefined);
     } else if (res.body.features.length === 0) {
